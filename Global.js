3dc512a7708f531c4f1fc6b3ae47f86aef84c8b1
gs <samsaausleipzig@gmail.com>
2014-04-20 16:17:25 -0300

    Add Global.js

---
+++ Global.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+var 	EOL = '/r/n',
+		ATTRIBUTE_TAG = 'attribute',
+		CLASS_TAG = 'class',
+		CODE_TAG = 'code',
+		DATATYPE_TAG = 'datatype',
+		DIAGRAM_TAG = 'diagram',
+		EVENT_TAG = 'event',
+		FINALIZATION_TAG = 'finalization',
+		IMPLEMENTATION_TAG = 'implementation',
+		INITIALIZATION_TAG = 'initialization',
+		MEMBER_TAG = 'member',
+		METHOD_TAG = 'method',
+		PARAMETER_TAG = 'parameter',
+		PROPERTY_TAG = 'property',
+		RELATIONSHIP_TAG = 'relationship',
+		SETUP_TAG = 'setup';
